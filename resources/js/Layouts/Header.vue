b718009bb99b940cc0b90216e710fb5505dc8ce6
IkerEscartin <iee0004@alu.medac.es>
2026-02-03 13:53:46 +0100

    Cronómetro básico con Laravel y Vue

---
+++ resources/js/Layouts/Header.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+  <header class="h-header bg-header flex justify-between p-5">
+    <h1 class="text-4xl text-blue-900">"PRUEBA DE VUE"</h1>
+    <form action="" method="post">
+      <button type="submit" class="btn btn-primary cursor-pointer">"Logout"</button>
+    </form>
+    <div>
+      <a><button class="btn btn-sm btn-primary">"Login"</button></a>
+      <a><button class="btn btn-sm btn-primary">"Register"</button></a>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Layouts/Layout.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Nav from "@/Layouts/Nav.vue";
+import Header from "@/Layouts/Header.vue";
+import Footer from "@/Layouts/Footer.vue";
+</script>
+
+<template>
+<Header/>
+  <Nav/>
+  <slot/>
+  <Footer/>
+</template>
+
+<style scoped>
+
+</style>
